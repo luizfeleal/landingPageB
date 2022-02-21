454518b1b56ef958c028d97d3764ecfe622c6ceb
Luiz Felipe Leal de Araujo <felipelearaujo@gmail.com>
2022-02-21 12:23:43 -0300

    criação de rotas

--- src/App.js
+++ src/App.js
@@ -1,31 +1,18 @@
-import React, { useState } from 'react';
+import React from 'react';
+import {Routes, Route} from 'react-router-dom';
 import './App.css';
-import Dropdown from './components/Dropdown';
-import Hero from './components/Hero';
-import InfoSection from './components/InfoSection';
-import Navbar from './components/Navbar';
-import StorySection from './components/StorySection';
-import { InfoData } from './data/infoData';
-import { StoryData } from './data/StoryData';
-import { SliderData } from './data/SliderData';
+import FirstPage from './pages/FirstPage';
 import GlobalStyle from './globalStyles';
-import Footer from './components/Footer';
-import { FooterData } from './data/FooterData';
 
 function App() {
-  const [isOpen, setIsOpen] = useState(false)
-  const toggle = () => {
-    setIsOpen(!isOpen)
-  }
   return (
     <>
     <GlobalStyle />
-      <Navbar toggle={toggle}/>
-      <Dropdown isOpen={isOpen} toggle={toggle}/>
-      <Hero slides={SliderData}/>
-      <InfoSection {...InfoData}/>
-      <StorySection {...StoryData}/>
-      <Footer {...FooterData}/>
+          <Routes>
+            <Route exact path='/' element={<FirstPage />}/>
+            <Route exact path='/rockinrio' element={<FirstPage />}/>
+          </Routes>
+          
     </>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import {BrowserRouter as Router} from 'react-router-dom';
+import {BrowserRouter} from 'react-router-dom';
 
 ReactDOM.render(
-    <Router>
-    <App />
-    </Router>,
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>,
   document.getElementById('root')
 );
 
