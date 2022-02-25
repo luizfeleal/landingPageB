3cf9172f22ab5aa4b6179a3546c01afc6486711a
Luiz Felipe Leal de Araujo <felipelearaujo@gmail.com>
2022-02-25 13:45:19 -0300

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import {Routes, Route} from 'react-router-dom';
 import './App.css';
 import FirstPage from './pages/FirstPage';
 import GlobalStyle from './globalStyles';
+import FestaCorporativa from './pages/FestaCorporativa';
+import RockInRio from './pages/RockInRio';
+import Casamento from './pages/Casamento';
 
 function App() {
   return (
@@ -10,7 +13,9 @@ function App() {
     <GlobalStyle />
           <Routes>
             <Route exact path='/' element={<FirstPage />}/>
-            <Route exact path='/rockinrio' element={<FirstPage />}/>
+            <Route exact path='/festaCorporativa' element={<FestaCorporativa />}/>
+            <Route exact path='/rockInRio' element={<RockInRio />}/>
+            <Route exact path='/casamento' element={<Casamento />}/>
           </Routes>
           
     </>
---
+++ src/Routes.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { Route, Switch } from 'react-router-dom';
+
+export default () => {
+    return (
+        <Switch>
+            <Route exact path="/">
+                Pagina inicial
+            </Route>
+            
+            <Route exact path="/">
+                Pagina inicial
+            </Route>
+        </Switch>
+    );
+}
